fc398785a005a854dab969420cd284157603c8fc
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-10-08 15:20:12 -0300

    projeto ????

---
+++ script/code.js
@@ -0,0 +1,11 @@
+function clique(){
+    var video = document.getElementById('video')
+    var botao = document.getElementById('bot')
+    if(video.style.display == 'block'){
+        video.style.display = 'none'
+    }
+    else{
+        video.style.display = 'block'
+    }
+
+}
