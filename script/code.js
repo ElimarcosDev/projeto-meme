30fff0b11448009de5b6c4ca8efcef22f1a2ab9d
Elimarcos De Souza Pereira <110783938+ElimarcosDev@users.noreply.github.com>
2022-10-09 10:05:16 -0300

    correção no player do video

--- script/code.js
+++ script/code.js
@@ -1,11 +1,17 @@
 function clique(){
+    
     var video = document.getElementById('video')
     var botao = document.getElementById('bot')
+    video.setAttribute('autoplay', 'i')
+
     if(video.style.display == 'block'){
         video.style.display = 'none'
     }
     else{
         video.style.display = 'block'
     }
+    
+
+    
 
 }
